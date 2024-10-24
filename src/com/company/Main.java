dab1cd92933cc36b0c5c4d2a72201a89844385ab
barisahmet kul <bariskul54@gmail.com>
2024-10-24 21:48:06 +0300

    class create odev 4

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Worker worker = new Worker();
+        worker.name = "Elif Nur Korkmaz Kul";
+        worker.department = "Information Technology";
+        worker.role = "Software Qa Engineer";
+        worker.title ="Sr";
+        worker.age=30;
+        worker.experience=7;
+        int prim=worker.primHesapla();
+        System.out.println(prim);
+    }
+}
---
+++ src/com/company/Worker.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Worker {
+
+        String name;
+        String department;
+        String role;
+        String title;
+        int age;
+        int experience;
+
+        int primHesapla (){
+            int prim= age +experience;
+
+            return prim;
+        }
+
+
+}
